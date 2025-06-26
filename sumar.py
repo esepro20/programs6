1f9033d6b141c24c2c8717671a748a33aed6e2d5
esepro20 <ese.rubio@duocuc.cl>
2025-06-26 14:22:05 -0400

    Add files via upload

--- sumar.py
+++ sumar.py
@@ -1,5 +1,10 @@
 def sumar(a,b):
     return
 
+def resta(a,b):
+    return a-b
+
 n1 = int(input("ingrese valor: "))
 n2 = int(input("ingrese valor: "))
+print("la sumna ed: ".sumar(n1,n2))
+print("la redts es: ".resta(n1,n2))
